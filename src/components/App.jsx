2a396d7ce123f180ea1e074328f828afa5b41f82
Oleksandr <alexander.tsimbaliuk52@gmail.com>
2023-05-09 13:29:37 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 // import { nanoid } from 'nanoid';
 import Form from './Form';
 import ContactsList from './ContactsList';
+import Filter from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
   handlerFormSubmit = contactData => {
@@ -19,6 +26,7 @@ export class App extends Component {
     return (
       <div>
         <Form onSubmit={this.handlerFormSubmit} title="Phonebook"></Form>
+        <Filter></Filter>
         <ContactsList
           contacts={this.state.contacts}
           title="Contacts"
